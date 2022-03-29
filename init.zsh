0cbe00cde1a50ff715dac4687a3695f4d6ef9eb9
hikari234 <hikari233@icloud.com>
2022-03-29 10:37:15 +0800

    modified:   init.zsh 	new file:   theme.zsh 	new file:   zplug.zsh

--- init.zsh
+++ init.zsh
@@ -1,36 +1,33 @@
-# Created by newuser for 5.8
+# 
+# My Zsh Config
 #
 ### +++++++++++ +++++++++++ +++++++++++ +++++++++++ +++++++++++ +++++++++++
 ###                            Daily shortcut
 ### +++++++++++ +++++++++++ +++++++++++ +++++++++++ +++++++++++ +++++++++++
 alias cs=cowsay
-alias d="g++ d.cc"
 alias nf=neofetch
 alias vi=nvim
 alias ra=ranger
-alias m="sudo make clean install"
 
+alias a="./a.out"
+alias m="sudo make clean install"
+alias pi="ping archlinux.org"
+alias shut="sudo shutdown -h 0"
+alias lsys="systemctl list-unit-files --type=service | grep enabled"
 
 alias n="nvim ~/.config/nvim/init.vim"
-alias z="nvim ~/.zshrc"
-alias s="source ~/.zshrc"
-
-# Lazy c++ code
-alias a="./a.out"
-
-# For net work
-alias pi="ping archlinux.org"
-alias lsys="systemctl list-unit-files --type=service | grep enabled"
+alias z="nvim ~/.config/zsh/init.zsh"
+alias s="source ~/.config/zsh/init.zsh"
 
 # output color 
 alias ls="ls --color"
 autoload -U colors && colors
 
 # prompt display
-eval "$(starship init zsh)"
+# eval "$(starship init zsh)"
 
 ### +++++++++++ +++++++++++ +++++++++++ +++++++++++ +++++++++++ +++++++++++
-###                            Environment ( Path )
+###                          Environment ( Path )
 ### +++++++++++ +++++++++++ +++++++++++ +++++++++++ +++++++++++ +++++++++++
 
 CLASSPATH=".:/usr/local/lib/antlr-4.9.2-complete.jar:$CLASSPATH"
@@ -53,11 +50,33 @@ net ()
 g ()
 {
     g++ $1
+    if [ $? -eq 0 ]; then
+        echo "Compile Successfully!"
+    else
+        echo "Compilation Error"
+    fi
     ./a.out
+    if [ $? -eq 0 ]; then
+        echo "Running Successfully!"
+    else
+        echo "Runtime Error"
+    fi
 }
 
-c ()
+v ()
 {
-    cp ~/algo/draft/sample.cc $1.cc
-    nvim $1.cc
+    if [ -f "$1" ]; then
+        echo "Opening $1..."
+        nvim $1
+    else
+        echo "Creating new file...\nFile name: $1"
+        cp ~/algo/draft/.sample.cc $1.cc
+        nvim $1.cc
+    fi
+}
+d ()
+{
+    g++ $1.cc
+    echo "Running..."
+    ./a.out
 }
